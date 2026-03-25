68197afeca0c225610534043452b91a27dc2c071
k2 <kkvs1805@gmail.com>
2026-03-25 20:52:26 +0800

    used try with resources

--- src/main/java/com/mycompany/crudapp1/CRUDApp1.java
+++ src/main/java/com/mycompany/crudapp1/CRUDApp1.java
@@ -21,32 +21,30 @@ public class CRUDApp1 {
         String user = "k2";
         String password = "k20518";
 
-        Connection conn = DriverManager.getConnection(url, user, password);
-        PreparedStatement pStatement1 = conn.prepareStatement("INSERT INTO players (name, time_in_sec) VALUES (?, ?)");
-        PreparedStatement statement =  conn.prepareStatement("SELECT * FROM players");
-        
-        //Handle placeholders (?)
-        pStatement1.setString(1, "frince");
-        pStatement1.setDouble(2, 24.2);
-        //statement.setInt(1, column);
-        
-        //Execute query
-        pStatement1.executeUpdate();
-        ResultSet resultSet = statement.executeQuery();
-        
-        System.out.println("id Name time_in_sec");
-        System.out.println("-----------------------------");
-        
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("name");
-            double time_in_sec = resultSet.getDouble("time_in_sec");
-            System.out.println(id + " " + name + " " + time_in_sec);
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            PreparedStatement pStatement1 = conn.prepareStatement("INSERT INTO players (name, time_in_sec) VALUES (?, ?)");
+            //Handle placeholders (?)
+            try (PreparedStatement statement = conn.prepareStatement("SELECT * FROM players")) {
+                //Handle placeholders (?)
+                pStatement1.setString(1, "frince");
+                pStatement1.setDouble(2, 24.2);
+                //statement.setInt(1, column);
+                
+                //Execute query
+                pStatement1.executeUpdate();
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    System.out.println("id Name time_in_sec");
+                    System.out.println("-----------------------------");
+                    
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt("id");
+                        String name = resultSet.getString("name");
+                        double time_in_sec = resultSet.getDouble("time_in_sec");
+                        System.out.println(id + " " + name + " " + time_in_sec);
+                    }
+                }
+            }
         }
-        
-        resultSet.close();
-        statement.close();
-        conn.close();
         
     }
 }
