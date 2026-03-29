dc8a03d1b1496f053e16e6469c645c7b68ab1638
k2 <kkvs1805@gmail.com>
2026-03-29 15:02:10 +0800

    player class

---
+++ src/main/java/com/mycompany/crudapp1/Player.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.crudapp1;
+
+/**
+ *
+ * @author k2
+ */
+public class Player {
+    private int id;
+    private String name;
+    private double time_in_sec;
+    
+    Player(int id, String name, double time_in_sec) {
+        this.id = id;
+        this.name = name;
+        this.time_in_sec = time_in_sec;
+    }
+}
